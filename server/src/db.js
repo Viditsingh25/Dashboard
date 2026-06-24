863b65312a85ec12b527cc8ad14f1c09f1b01d83
Viditsingh25 <128414585+Viditsingh25@users.noreply.github.com>
2026-06-24 22:56:58 +0530

    enable ssl for supabase connection

--- server/src/db.js
+++ server/src/db.js
@@ -8,6 +8,7 @@ const pool = new pg.Pool({
   database: process.env.PGDATABASE || "kims_dashboard",
   user: process.env.PGUSER || "postgres",
   password: process.env.PGPASSWORD || "postgres",
+  ssl: process.env.PGHOST !== "localhost" ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("error", (err) => {
